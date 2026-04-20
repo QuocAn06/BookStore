5b2f5d4344734849664d287a954dde596b1379e9
Le Nguyen Quoc An <55307642+QuocAn06@users.noreply.github.com>
2026-04-21 00:51:21 +0700

    feat: Add login/logout links in layout

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -40,7 +40,7 @@ namespace BookStore.Controllers
 
             if (result.Succeeded)
             {
-                await _signInManager.SignInAsync(user, isPersistent: false;
+                await _signInManager.SignInAsync(user, isPersistent: false);
                 return RedirectToAction("Index", "Home");
             }
 
